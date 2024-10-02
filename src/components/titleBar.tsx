d3e405c10ed011242f4937f20576850be507650e
José Gomes de Menezes <josegomesd6@gmail.com>
2024-10-02 19:26:31 -0300

    :feat: feat: dashboard + cards + button

--- src/components/titleBar.tsx
+++ src/components/titleBar.tsx
@@ -10,12 +10,15 @@ interface titleBarProps {
 
 export default function TitleBar({ labelNav, nameUser }: titleBarProps) {
     return (
-        <header className="title-bar flex h-32 px-10 bg-gray-200 items-center justify-between">
+        <header className="title-bar flex h-32 px-[68px] border-b-2 border-gray-200 items-center justify-between">
+            
             <h1 className="m-0">{labelNav}</h1>
+
             <nav className="flex items-center gap-2">
+
                 <Button className="gap-2" variant="outline">
-                    <Avatar className="w-6 h-6 "> {/* Ajuste o tamanho aqui */}
-                        <AvatarImage src="https://github.com/shadcn.png" />
+                    <Avatar className="w-6 h-6 "> 
+                        <AvatarImage src="https://avatars.githubusercontent.com/u/142457990?s=400&u=8e952a59e833927839056c50a6cab0e514e07688&v=4" />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
                     {nameUser}
@@ -24,7 +27,9 @@ export default function TitleBar({ labelNav, nameUser }: titleBarProps) {
                 <Button className="bg-[var(--bg-color-main)]">
                     Logout <LogOut className="ml-2 h-4 w-4" />
                 </Button>
+                
             </nav>
+
         </header>
     )
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,51 @@
-import { Headphones, LayoutDashboard, HandCoins, ArrowDownUp, Forklift, User, Settings } from 'lucide-react';
+import { Headphones, LayoutDashboard, HandCoins, ArrowDownUp, Forklift, User, Settings, DollarSign } from 'lucide-react';
 import SidebarItem from '@/components/sideBarItem';
 import TitleBar from '@/components/titleBar';
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import { Download } from 'lucide-react';
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Card, CardHeader, CardDescription, CardContent, CardTitle } from '@/components/ui/card';
+import { Button } from "@/components/ui/button"
+
 
 export default function DashBoard() {
+
+    const chartData = [
+        { month: "Janeiro", revenue: 10.159, customers: 80 },
+        { month: "Fevereiro", revenue: 24.000, customers: 200 },
+        { month: "Março", revenue: 18.000, customers: 120 },
+        { month: "Abril", revenue: 19.356, customers: 190 },
+        { month: "Maio", revenue: 19.984, customers: 130 },
+        { month: "Junho", revenue: 21.234, customers: 140 },
+    ];
+
+    // Pega os últimos três meses
+    const lastThreeMonths = chartData.slice(-3);
+
+    // Calcula o total de revenue dos últimos três meses
+    const totalRevenueLastThreeMonths = lastThreeMonths.reduce((acc, item) => acc + item.revenue, 0);
+
+    // Calcula o total de clientes dos últimos três meses
+    const totalCustomersLastThreeMonths = lastThreeMonths.reduce((acc, item) => acc + item.customers, 0);
+
+    const chartConfig = {
+        revenue: {
+            label: "Receita",
+            color: "#909497",
+        },
+        customers: {
+            label: "Clientes",
+            color: "var(--bg-color-main)",
+        },
+    } satisfies ChartConfig;
+
+
+
     return (
         <div className="h-screen text-white flex">
 
@@ -12,7 +55,7 @@ export default function DashBoard() {
                     <SidebarItem Icon={LayoutDashboard} label="Dashboard" href="#" />
                     <SidebarItem Icon={HandCoins} label="Precificação" href="#" />
                     <SidebarItem Icon={ArrowDownUp} label="Fluxo de caixa" href="#" />
-                    <SidebarItem Icon={Forklift} label="Cadastro" href="#" />
+                    <SidebarItem Icon={Forklift} label="Estoque/Cadastro" href="#" />
                     <SidebarItem Icon={Headphones} label="Suporte" href="#" />
                 </nav>
 
@@ -24,7 +67,103 @@ export default function DashBoard() {
             </aside>
 
             <main className="flex-1 bg-slate-50 text-black">
-                <TitleBar labelNav={'DashBoard'} nameUser='Caio Souza'/>
+
+                {/* Header da main */}
+                <TitleBar labelNav={'DashBoard'} nameUser='Caio Souza' />
+
+                {/* Conteudo da main */}
+                <div className='w-11/12 m-auto'>
+
+                    <div className='flex items-center justify-between'>
+                        <h2 className='text-3xl font-bold m-8'>Relatorio </h2>
+                        <Button variant="outline" className='gap-2'>
+                            <Download /> Exportar
+                        </Button>
+                    </div>
+
+                    <div className='grid grid-cols-2 gap-4'>
+                        {/* Dashboard div */}
+                        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+                            <BarChart data={chartData}>
+                                <CartesianGrid vertical={true} />
+
+                                {/* Eixo X */}
+                                <XAxis
+                                    dataKey="month"
+                                    tickLine={false}
+                                    tickMargin={10}
+                                    axisLine={false}
+                                    tickFormatter={(value) => value.slice(0, 3)}
+                                />
+
+                                {/* Eixo Y para revenue */}
+                                <YAxis yAxisId="left" orientation="left" />
+
+                                {/* Eixo Y para customers */}
+                                <YAxis yAxisId="right" orientation="right" />
+
+                                <ChartTooltip content={<ChartTooltipContent />} />
+
+                                {/* Barra para revenue */}
+                                <Bar yAxisId="left" dataKey="revenue" fill="var(--color-revenue)" radius={4} />
+
+                                {/* Barra para customers */}
+                                <Bar yAxisId="right" dataKey="customers" fill="var(--color-customers)" radius={4} />
+                            </BarChart>
+
+                        </ChartContainer>
+
+                        {/* Cards column */}
+                        <div className="grid grid-rows-2 gap-4">
+                            {/* Total em vendas */}
+                            <Card>
+                                <CardHeader>
+                                    <div className="flex items-center justify-center">
+                                        <CardTitle className='text-lg text-gray-800 select-none'>
+                                            Total em vendas
+                                        </CardTitle>
+                                        <DollarSign className='ml-auto w-4 h-4' />
+                                    </div>
+
+                                    <CardDescription>
+                                        Total vendas em 90 dias
+                                    </CardDescription>
+                                </CardHeader>
+
+                                <CardContent>
+                                    <p className='text-base font-bold'>
+                                        R${totalRevenueLastThreeMonths.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                                    </p>
+
+                                </CardContent>
+                            </Card>
+
+
+                            {/* Card Taxa de clientes */}
+                            <Card>
+                                <CardHeader>
+                                    <div className="flex items-center justify-center">
+                                        <CardTitle className='text-lg text-gray-800 select-none'>
+                                            Total de clientes
+                                        </CardTitle>
+                                        <User className='ml-auto w-4 h-4' />
+                                    </div>
+
+                                    <CardDescription>
+                                        Clientes nos últimos 90 dias
+                                    </CardDescription>
+                                </CardHeader>
+
+                                <CardContent>
+                                    <p className='text-base font-bold'>{totalCustomersLastThreeMonths}</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+
+                    </div>
+
+
+                </div>
             </main>
         </div>
     );
