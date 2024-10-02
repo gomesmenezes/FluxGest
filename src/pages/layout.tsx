efdbd1fa8d29e10c3e69546364ba96c34618585c
José Gomes de Menezes <josegomesd6@gmail.com>
2024-10-02 11:10:52 -0300

    :mag: mag: favicon add

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,24 +1,25 @@
 import Header from '@/components/header';
-import '../styles/globals.css'; 
+import '../styles/globals.css';
 
 export const metadata = {
     title: 'FluxGest',
-    description: 'Organize sua vida financeira.' 
+    description: 'Organize sua vida financeira.'
 }
 
 export default function RootLayout({
-    children, 
+    children,
 }: {
-    children: React.ReactNode 
+    children: React.ReactNode
 }) {
     return (
-        <html lang="pt-BR"> {}
+        <html lang="pt-BR"> { }
             <head>
+                <link rel="icon" href="/public/favicon.jpg" type="image/jpeg" />
                 <title>{metadata.title}</title>
                 <meta name="description" content={metadata.description} />
             </head>
             <body>
-                <Header/>
+                <Header />
                 {children}
             </body>
         </html>
