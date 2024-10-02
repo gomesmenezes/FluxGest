3551f1cf035db4695b789712875133c469059a74
José Gomes de Menezes <josegomesd6@gmail.com>
2024-10-01 23:23:23 -0300

    :sparkles: feat: Label user add

--- src/components/titleBar.tsx
+++ src/components/titleBar.tsx
@@ -1,6 +1,8 @@
 import { EnvelopeOpenIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 import { LogOut } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 
 interface titleBarProps {
@@ -12,11 +14,18 @@ export default function TitleBar({ labelNav }: titleBarProps) {
     return (
         <header className="title-bar flex h-32 px-10 bg-gray-200 items-center justify-between">
             <h1 className="m-0">{labelNav}</h1>
-            <nav>
+            <nav className="flex items-center gap-2">
+                <Button className="gap-2" variant="outline">
+                    <Avatar className="w-6 h-6 "> {/* Ajuste o tamanho aqui */}
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    Name User
+                </Button>
+
                 <Button className="bg-[var(--bg-color-main)]">
-                    
-                    Logout <LogOut className="ml-2 h-4 w-4" /> 
-                </Button> 
+                    Logout <LogOut className="ml-2 h-4 w-4" />
+                </Button>
             </nav>
         </header>
     )
